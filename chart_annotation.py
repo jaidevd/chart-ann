26b469eac50f1663b7e9652661c595fb051940b2
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-09-11 13:45:38 +0530

    Look at images only when GETting

--- chart_annotation.py
+++ chart_annotation.py
@@ -2,8 +2,9 @@ from base64 import urlsafe_b64decode
 
 
 def view(data, path, handler):
-    handler.set_header('Content-Type', 'image/png')
-    handler.set_header('Content-Disposition', 'attachment; filename=image.png')
-    url = data.iloc[0]['image'].split(',')[1]
-    data = urlsafe_b64decode(url)
-    return data
+    if handler.request.method == 'GET':
+        handler.set_header('Content-Type', 'image/png')
+        handler.set_header('Content-Disposition', 'attachment; filename=image.png')
+        url = data.iloc[0]['image'].split(',')[1]
+        data = urlsafe_b64decode(url)
+        return data

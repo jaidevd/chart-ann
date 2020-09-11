d7e4df619fa24f85230c92c07558eaf9e5581bbe
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-09-11 13:58:19 +0530

    Misc deployment changes

--- chart_annotation.py
+++ chart_annotation.py
@@ -1,10 +1,12 @@
 from base64 import urlsafe_b64decode
+from gramex.data import filter as gfilter
+from gramex.config import variables
 
 
-def view(data, path, handler):
-    if handler.request.method == 'GET':
-        handler.set_header('Content-Type', 'image/png')
-        handler.set_header('Content-Disposition', 'attachment; filename=image.png')
-        url = data.iloc[0]['image'].split(',')[1]
-        data = urlsafe_b64decode(url)
-        return data
+def view(handler):
+    handler.set_header('Content-Type', 'image/png')
+    handler.set_header('Content-Disposition', 'attachment; filename=image.png')
+    data = gfilter(variables['DB_URL'], table='charts', args={'chart_id': [handler.path_args[0]]})
+    url = data.iloc[0]['image'].split(',')[1]
+    data = urlsafe_b64decode(url)
+    return data
